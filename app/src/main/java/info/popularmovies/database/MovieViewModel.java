c8c984ce91db4fbfa906d11af9ca393e885a457b
joshNic <256jomu@gmail.com>
2018-10-14 20:11:40 +0300

    add add favourite movies
    Signed-off-by: joshNic <256jomu@gmail.com>

--- app/src/main/java/info/popularmovies/database/MovieViewModel.java
+++ app/src/main/java/info/popularmovies/database/MovieViewModel.java
@@ -10,18 +10,32 @@ public class MovieViewModel extends AndroidViewModel {
     private MoviesRepository mRepository;
 
     private LiveData<List<DatabaseMovie>> mAllWords;
+    private LiveData<DatabaseMovie> favouriteMovie;
+    //Movie id
+    private int mId;
 
     public MovieViewModel(Application application) {
         super(application);
         mRepository = new MoviesRepository(application);
+        favouriteMovie = mRepository.getFavouriteMovieById(mId);
         mAllWords = mRepository.getAllWords();
+
     }
 
     LiveData<List<DatabaseMovie>> getAllWords() {
         return mAllWords;
     }
 
+    public LiveData<DatabaseMovie> getFavouriteMovieById(int id) {
+        return favouriteMovie;
+    }
+
     public void insert(DatabaseMovie word) {
         mRepository.insert(word);
     }
+
+    public void delete(DatabaseMovie word) {
+        mRepository.delete(word);
+    }
+
 }
